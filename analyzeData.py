0ec5bfc78264a2c55c88a0a5b90f60a2b8f58817
Zach Nowak <zacandcheese@icloud.com>
2017-11-01 01:02:48 -0400

    Added a test way to visualize data

---
+++ analyzeData.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob, os
+from itertools import cycle
+
+os.chdir("/Users/zacan/OneDrive/Documents/GitHub/Keyboard-Biometric-Testing")
+
+listOfTxtFiles = []
+for file in glob.glob("*.txt"):
+	#print(file)
+	try:
+		np.loadtxt(file, delimiter=",", unpack=True)
+		listOfTxtFiles.append(file)
+	except ValueError:
+		pass
+
+
+lines = ["-","--","-.",":"]
+linecycler = cycle(lines)
+for i in range(len(listOfTxtFiles)):
+	plt.figure(1)
+	array = np.loadtxt(listOfTxtFiles[i], delimiter=",", unpack=True)
+	plt.plot(array[0], array[2], next(linecycler), label = listOfTxtFiles[i])# array[0], array[4], 'ro' USE FOR AVG FLIGHT TIME
+plt.legend()
+plt.show()
