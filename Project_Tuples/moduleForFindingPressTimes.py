0365923869627210e901927a0fe7b10e1846e41b
Zach Nowak <zacandcheese@icloud.com>
2017-11-05 16:13:17 -0500

    Added Comments

--- Project_Tuples/moduleForFindingPressTimes.py
+++ Project_Tuples/moduleForFindingPressTimes.py
@@ -5,20 +5,27 @@ from statistics import *
 
 def create_dict(pressCharTimeLine,pressTimeLine,releaseCharTimeLine, releaseTimeLine, dataDict):
 	
-	#FIND UNIQUE CHARACTERS
+	"""FIND NUMBER OF UNIQUE CHARACTERS"""
 	runningHistory = ""
 	for char in pressCharTimeLine:
 		if char not in runningHistory:
 			runningHistory+=char
 	numUniqueChar = len(runningHistory)-1#DO NOT COUNT THE /n
 	pressTimingList = [[] for i in range(numUniqueChar)]
+	
+	"""FIND THE PRESS TIMES FOR EACH LETTER"""
 	for i in range(len(pressCharTimeLine)-1):
+		#The function finds the initial press time in the timeline then it subtracts
+		#that from the release time and that gives the total press time.
 		char = pressCharTimeLine[i]
-		j = releaseCharTimeLine.index(char, i)
+		j = releaseCharTimeLine.index(char, i)# The i is used to find the char after the press because, otherwise it would just index the first one.
 		charIndex = runningHistory.index(char)
 		pressTimingList[charIndex].append(releaseTimeLine[j] - pressTimeLine[i])
+		
+	"""ASSIGN THE LETTER WITH IT'S MEDIAN TOTAL PRESS TIME"""	
 	for i in range(len(pressTimingList)):
 		char = runningHistory[i]
 		list = pressTimingList[i]
-		dataDict[char] = median(list) 
+		dataDict[char] = median(list)
+		
 	return dataDict
--- Project_Tuples/moduleForFindingTuplesTime.py
+++ Project_Tuples/moduleForFindingTuplesTime.py
@@ -5,20 +5,24 @@ from statistics import *
 
 def create_dict(tupleList,pressCharTimeLine,pressTimeLine,dataDict):
 	keyHistory = ""
-	timingList = [[] for i in range(len(tupleList))]#FOR TIME IT TAKES TO WRITE A WORD
+	timingList = [[] for i in range(len(tupleList))]
+	
+	"""CREATE A STRING WITH ALL THE EVENTS"""
 	for char in pressCharTimeLine:
 		keyHistory += char
+	
+	"""FIND THE TIME IT TAKES TO TYPE EACH WORD FROM PRESS TO PRESS"""
 	for string in tupleList:
 		i = 0
 		index = tupleList.index(string)
 		while(keyHistory.find(string.upper(), i))!= -1:
 			position = keyHistory.find(string.upper(),i)
-			i = position+len(tupleList[0])
+			i = position + len(tupleList[0])
 			timingList[index].append(pressTimeLine[i - 1] - pressTimeLine[position])
 	
+	"""ASSIGN THE TUPLE WITH IT'S MEDIAN TOTAL PRESS TIME"""
 	i = 0
 	for tuple in timingList:
-		#print("The history is: ", tuple)
 		print("The median is: ", median(tuple))
 		print("The mean is: ", harmonic_mean(tuple))
 		dataDict[tupleList[i]] = median(tuple)
