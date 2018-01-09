4178ec7662e0672a872d9992febeaeff4e8fa067
finsmatt <firstplacematt@icloud.com>
2018-01-09 10:53:49 -0500

    Tried to fix a issue in code

--- Project_Tuples/WillDelete.py
+++ Project_Tuples/WillDelete.py
@@ -17,9 +17,9 @@ print(listOfTxtFiles)
 pressListFile = open(listOfTxtFiles[0],"r").readlines()
 pressList = []
 for pInt in pressListFile:
-	pressList.append(int(pInt[0:-9])+int(pInt[11:])*10**-(len(pInt)-13))
+	pressList.append(int(pInt[0:-9])+int(pInt[11:-1])*10**-(len(pInt)-13))
 
-#CharList	
+#CharList
 charListFile = open(listOfTxtFiles[1],"r").readlines()
 charList = []
 for pChar in charListFile:
