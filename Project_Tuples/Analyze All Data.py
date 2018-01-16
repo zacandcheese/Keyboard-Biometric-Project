9aa4b48509a39f942c618d17cb8d9ecf23d154b7
finsmatt <firstplacematt@icloud.com>
2018-01-16 11:01:11 -0500

    New Stuff

---
+++ Project_Tuples/Analyze All Data.py
@@ -0,0 +1,50 @@
+import glob
+import os
+import statistics
+
+os.chdir("library/timelines/summary")
+
+listOfTxtFiles = []
+for file in glob.glob("*.txt"):
+	listOfTxtFiles.append(file)
+print(listOfTxtFiles)
+
+def newData(list):
+   megaList = []
+   for file in list:
+      for line in open(file,"r").readlines():#open the file, read all lines, add each line to the megalist
+         megaList.append(line)
+   #for line in megaList:
+   #   print(line.split(","))
+   #print(megaList)
+   
+   tuplesSeen = []
+   for line in megaList:
+      tuple = line.split(",")[0]#tuple
+      
+      if tuple not in tuplesSeen:
+         tuplesSeen.append(tuple)
+         dummyList = []
+         for line in megaList:#searches
+            tuple2 = line.split(",")[0]
+            if(tuple == tuple2):
+               dummyList.append(line.split(",")[3])
+               
+         if(len(dummyList)>=2):
+            print(tuple, statistics.variance(dummyList))
+      
+   
+      
+   """
+   find the tuple for all .txt files
+   if there is multiple of each, then
+   compare each of the text files 
+   print the tuple and variance
+   """
+   
+newData(listOfTxtFiles)
+
+   
+   
+   
+
